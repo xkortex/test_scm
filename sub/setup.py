6aeead2454d5569f4b878b1f895ff62f0d463707
Michael McDermott <michael.mcdermott@kitware.com>
2020-11-11 15:51:56 -0500

    experimenting with sub packages. works but only with python setup.py, pip errors out

--- sub/setup.py
+++ sub/setup.py
@@ -1,14 +1,14 @@
 from setuptools import find_packages, setup
 
-packages = find_packages()
 setup(
-    name='test_scm',
+    name='test_scm.sub',
     use_scm_version={
         'write_to': 'test_scm/_version.py',
         'write_to_template': '__version__ = "{version}"',
+        'root': '.',
+        # 'relative_to': '__file__',
     },
     script_name='setup.py',
     python_requires='>=3.6',
-    packages=packages,
     setup_requires=['setuptools_scm'],
 )
---
+++ sub/sub/howdy.py
@@ -0,0 +1,9 @@
+from test_scm.sub import __version__
+
+
+def main():
+    print("howdy from test_scm iter {} version {}".format(1, __version__))
+
+
+if __name__ == "__main__":
+    main()
